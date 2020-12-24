b370cea5dabdd0b58d630c9d66f9f7415a30305a
Wesley Osborne <wesleyosborne1@gmail.com>
2020-12-24 09:29:55 -0500

    Initial Commit

---
+++ Checklists/View Controllers/ItemDetailViewController.swift
@@ -0,0 +1,101 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Wesley Osborne on 11/29/20.
+//
+
+import UIKit
+import UserNotifications
+
+protocol ItemDetailViewControllerDelegate: class {
+  func itemDetailViewControllerDidCancel(_ controller: ItemDetailViewController)
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem)
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem)
+}
+
+class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  @IBOutlet var shouldRemindSwitch: UISwitch!
+  @IBOutlet var datePicker: UIDatePicker!
+  var itemToEdit: ChecklistItem?
+  
+  weak var delegate: ItemDetailViewControllerDelegate?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.largeTitleDisplayMode = .never
+    
+    if let itemToEdit = itemToEdit {
+      title = "Edit Item"
+      textField.text = itemToEdit.text
+      doneBarButton.isEnabled = true
+      shouldRemindSwitch.isOn = itemToEdit.shouldRemind
+      datePicker.date = itemToEdit.dueDate 
+    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+  
+  // MARK: - Actions
+  @IBAction func cancel() {
+    delegate?.itemDetailViewControllerDidCancel(self)
+  }
+  
+  @IBAction func done() {
+    
+    if let item = itemToEdit {
+      item.text = textField.text!
+      
+      item.shouldRemind = shouldRemindSwitch.isOn
+      item.dueDate = datePicker.date
+      item.scheduleNotification()
+      delegate?.itemDetailViewController(self, didFinishEditing: item)
+    } else {
+      let item = ChecklistItem(text: textField.text!)
+      item.text = textField.text!
+      item.checked = false
+      
+      item.shouldRemind = shouldRemindSwitch.isOn
+      item.dueDate = datePicker.date
+      item.scheduleNotification()
+      delegate?.itemDetailViewController(self, didFinishAdding: item)
+    }
+  }
+  
+  @IBAction func shouldRemindToggled(_ switchControl: UISwitch) {
+    textField.resignFirstResponder()
+    
+    if switchControl.isOn {
+      let center = UNUserNotificationCenter.current()
+      center.requestAuthorization(options: [.alert, .sound]) {_, _ in
+        // do nothing
+      }
+    }
+  }
+  // MARK: - Table View Delegates
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
+  }
+  
+  // MARK: - Text Field Delegates
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    let oldText = textField.text!
+    let stringRange = Range(range, in: oldText)!
+    let newText = oldText.replacingCharacters(in: stringRange, with: string)
+    
+    doneBarButton.isEnabled = !newText.isEmpty
+    
+    return true
+  }
+  
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
+  }
+  
+}
